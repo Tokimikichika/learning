eafe141a1a54f26d661381b0517688851705eb0b
Tokimikichika <Ahileses@mail.ru>
2022-06-09 21:36:27 +0200

    index.js

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
-const openTokens = ['(', '[', '{'];
+// const openTokens = ['(', '[', '{'];
 
-const closeTokens = [')', ']', '}'];
+// const closeTokens = [')', ']', '}'];
 
-let stack = [];
+// let stack = [];
 
-const push = () => {
-    stack.push();
-}
-const pop = () => {
-    return stack.pop();
-}
+// const push = () => {
+//     stack.push();
+// }
+// const pop = () => {
+//     return stack.pop();
+// }
 
-const isBalanced = str => {
+// const isBalanced = str => {
     
 
-    for (let i = 0; i < str.length; i++) {
-        let brackets = str[i];
-        if (brackets === openTokens) {
-            push(brackets);
-        } else if (brackets === closeTokens) {
-            pop(brackets);
+//     for (let i = 0; i < str.length; i++) {
+//         let brackets = str[i];
+//         if (brackets === openTokens) {
+//             push(brackets);
+//         } else if (brackets === closeTokens) {
+//             pop(brackets);
             
-        }
+//         }
         
-    }
-        return !stack.length;
-    }
-console.log(isBalanced('()[]{}'));
-console.log(isBalanced('[{()}]'));
-console.log(isBalanced('[)}'));
+//     }
+//         return !stack.length;
+//     }
+// console.log(isBalanced('()[]{}'));
+// console.log(isBalanced('[{()}]'));
+// console.log(isBalanced('[)}'));
 
-        
+class Steck {
+    constructor(el) {
+        this.el = el;
+        }
+}
+const steck = new Steck;
+let el = steck;
+el.push(10);
+el.push(20);
+
+el.pop(); //el === 20
